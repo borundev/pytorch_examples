a4542646887cc5cbbe1b7ca446e8b4001f63e857
borundev <borundev@gmail.com>
2021-01-31 09:02:57 +0100

    Using the new transfer learning module

--- pytorch_examples/classification_with_uncertainity/main.py
+++ pytorch_examples/classification_with_uncertainity/main.py
@@ -1,33 +1,65 @@
+import os
+
+import names
 import pytorch_lightning as pl
+import wandb
 from pytorch_lightning.loggers import WandbLogger
 
-from data.cats_and_dogs import CatsAndDogsDataModule
-from data.fudge_labels import  modify_data_module
-from models.resnet import CustomModel
-import wandb
+from data.fudge_labels import modify_data_module
+from data.kaggle import TBDataModule
+from pytorch_examples.transfer_learning.model import CustomModel
 
-CustomModel.make_validation_epoch_end(['C','D'])
+CustomModel.make_validation_epoch_end(['Normal', 'TB'])
 
 
-def run_with_mod(num_extras, name=None, max_epochs=5):
+if 'KAGGLE_USERNAME' not in os.environ:
+    os.environ['KAGGLE_USERNAME'] = input('Username: ')
+    os.environ['KAGGLE_KEY'] = input('Key: ')
+
+
+def run_with_mod(num_extras, name=None):
     #pl.seed_everything(42)
+    name = '{}_{}'.format(num_extras, '_'.join(names.get_full_name().split()))
     wandb_logger = WandbLogger(name=name,
-                               project='uncertain_classification2')
+                               project='uncertain_classification_transfer_learning')
+
+    model = CustomModel()
+    dm = modify_data_module(TBDataModule(),num_extras)
+    dm.prepare_data()
+    dm.setup()
+
+    model.steps_per_epoch = len(dm.train_dataloader())
+
+    freeze_max_epochs = 1
+    unfreeze_max_epochs = 5
+
+    model.epochs = freeze_max_epochs
+
+    model.freeze()
+
     trainer = pl.Trainer(
         gpus=0,
-        max_epochs=max_epochs,
-        logger=wandb_logger,
-        limit_train_batches=10,
-        limit_val_batches=5
-        )
-    model = CustomModel()
-    dm = modify_data_module(CatsAndDogsDataModule(),num_extras)
+        max_epochs=freeze_max_epochs,
+        logger=wandb_logger
+    )
+    trainer.fit(model, dm)
 
+
+    model.epochs=unfreeze_max_epochs
+    model.unfreeze()
+
+    trainer = pl.Trainer(
+        gpus=0,
+        max_epochs=freeze_max_epochs + unfreeze_max_epochs,
+        logger=wandb_logger
+    )
+    trainer.current_epoch = freeze_max_epochs
+    trainer.global_step = model.steps_per_epoch * freeze_max_epochs
     trainer.fit(model, dm)
 
     wandb.finish()
 
 if  __name__=='__main__':
-    run_with_mod(2,max_epochs=10)
-    #cdm = GetExtraLabelsDataModule(CatsAndDogsDataModule(), 4)
-    #x,y
+    for num_extras in (0,1,2,5,15):
+        run_with_mod(num_extras)
+
