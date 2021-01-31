13663ca4c7ed872177d7f350fe3a796c9d573c63
borundev <borundev@gmail.com>
2021-02-01 00:04:22 +0100

    Check the behavior with the optional inclusion of original label.

--- data/fudge_labels/main.py
+++ data/fudge_labels/main.py
@@ -1,12 +1,15 @@
 from data.cats_and_dogs.cats_and_dogs import CatsAndDogsDataModule
 from data.fudge_labels import  modify_data_module
+from data.kaggle import TBDataModule
+import numpy as np
 
-dm = modify_data_module(CatsAndDogsDataModule(), 4)
+dm = modify_data_module(TBDataModule(), 10, return_original=True)
 dm.prepare_data()
 dm.setup()
 train = dm.train_dataloader()
-x, y = next(iter(train))
-print(y)
+x, y, y_original = next(iter(train))
+print(np.corrcoef(y.numpy(),y_original.numpy()))
+print(list(zip(y_original,y)))
 
 val = dm.val_dataloader()
 x, y = next(iter(val))
